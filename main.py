19332f46f341b54fa1e848305a805b3f67329949
Daniel Wang <danielwang@Daniels-MacBook-Air.local>
2022-08-03 20:16:47 -0700

    upload data

---
+++ main.py
@@ -0,0 +1,48 @@
+# This is a sample Python script.
+import json
+import pandas as pd
+
+data = {'year':  [],
+        'county': [],
+        'zipcode': [],
+        'rate': [],
+        'num1': [],
+        'num2': [],
+        'num3': []
+        }
+
+df = pd.DataFrame(data)
+year = 2021
+
+def print_df():
+    # Use a breakpoint in the code line below to debug your script.
+    print(len(df))
+    #for d in df.iloc:
+    #    print(d)
+    df.to_csv(r'C:/projects/python/cdph/2021_ca.csv', index=False)
+
+def parseCounty(county, sjson):
+    y = json.loads(sjson)
+    for r in y["data"]:
+        df.loc[len(df.index)] = [year, county.strip(), r[0], r[1], r[2], r[3], r[4]]
+
+def parseFile():
+    # opening file in reading mode
+    lastLine = ""
+    with open('C:/projects/python/cdph/ca2021.txt', 'r') as fileobj:
+        while (True):
+            line = fileobj.readline()
+            if not line:
+                break;
+            if line.startswith("{") :
+                parseCounty(lastLine,line)
+            lastLine = line
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    parseFile()
+    print_df()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
